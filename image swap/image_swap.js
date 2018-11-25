96a6c99b3e2918a93e5dfd4766d58c68e478355e
Zachary Murphy <ztmurphy21@gmail.com>
2018-11-24 18:18:48 -0600

    image swapper web app

---
+++ image swap/image_swap.js
@@ -0,0 +1,39 @@
+$ = function (id){
+	return document.getElementById(id);
+}
+window.onload = function (){
+	var listNode = $("image_list"); //ul element
+	var captionNode = $("caption"); //h2 element
+	var imageNode = $("image"); //main img element
+	var imageLinks = listNode.getElementsByTagName("a"); // create array of a elements within the image list and save it in this variable
+	
+	//process img links
+	var i, linkNode, image;
+	for (i = 0; i < imageLinks.length; i++){
+		linkNode = imageLinks[i];
+		
+		//attach event handler
+		linkNode.onclick = function (evt){
+			//use 'this' keyword so a diff event handler is applied to each link 
+			var link = this;
+			//change src attribute in the images node to the href attribute of the link
+			imageNode.src = link.getAttribute("href");
+			
+			//nchange value of the first child of the caption node to the title attribute of the link
+			captionNode.firstChild.nodeValue = link.getAttribute("title");
+			
+			//cancel default action of event
+			if (!evt) evt = window.event;
+			if (evt.preventDefault) {
+				evt.preventDefault(); //dom complain code
+			}
+			else{
+				evt.returnValue = false;
+			}
+		}
+		//preload images
+		image = new Image();
+		image.src = linkNode.getAttribute("href");
+	}
+	$("first_link").focus();
+} 
